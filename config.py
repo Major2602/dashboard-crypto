7cc684ec1bc68048d5f76e6f3559a3aa577d8171
Major2602 <shdm0023@gmail.com>
2026-08-01 02:23:59 +0500

    Change QUOTES_PATH and REPORT_PATH to relative paths
    Updated file paths for quotes and reports to relative paths.

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
   
     TARGET_TICKERS = ['BTC', 'ETH', 'XRP', 'SOL', 'ADA']
   
-    QUOTES_PATH = '/content/drive/MyDrive/cr_quotes_base.csv'
+    QUOTES_PATH = 'data/cr_quotes_base.csv'
   
-    REPORT_PATH = '/content/drive/MyDrive/cr_report.csv'
+    REPORT_PATH = 'data/cr_report.csv'
   
     TICKER_COLORS = {t:  px.colors.qualitative.Plotly[i % len( px.colors.qualitative.Plotly)] for i, t in enumerate(TARGET_TICKERS)}
