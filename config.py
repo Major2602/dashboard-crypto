fcbe19876e394aa478938a40e232fcbb1b3c987c
Major2602 <shdm0023@gmail.com>
2026-08-01 03:03:11 +0500

    Update quotes and report paths in config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
   
     TARGET_TICKERS = ['BTC', 'ETH', 'XRP', 'SOL', 'ADA']
   
-    QUOTES_PATH = 'data/cr_quotes_base.csv'
+    QUOTES_PATH = 'data/cr_quotes_base_24.csv'
   
-    REPORT_PATH = 'data/cr_report.csv'
+    REPORT_PATH = 'data/cr_report_24.csv'
   
     TICKER_COLORS = {t:  px.colors.qualitative.Plotly[i % len( px.colors.qualitative.Plotly)] for i, t in enumerate(TARGET_TICKERS)}
