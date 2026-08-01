5f7c1f6eb660cc1a1f4fd19c5e8c3c520d0864fa
Major2602 <shdm0023@gmail.com>
2026-08-01 16:43:32 +0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-from layout import app
-import callbacks
-
-server = app.server
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-    app.run_server(debug=False, host="0.0.0.0", port=port)
