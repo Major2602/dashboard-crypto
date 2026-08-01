d9325e140beb00b17f795edb68fe7f984fa53f2e
Major2602 <shdm0023@gmail.com>
2026-08-01 13:50:09 +0500

    Update date index normalization in df_q
    Normalize the date index in df_q to remove time component.

--- data_layer.py
+++ data_layer.py
@@ -4,7 +4,6 @@ import pandas as pd
 import ast
 from typing import Tuple
 from config import Config
-import datetime as dt
 
 
 class DataManager:
@@ -20,7 +19,7 @@ class DataManager:
 
         df_r['tickers_dict'] = df_r['tickers_count'].apply(safe_parse_dict)
         df_r['date'] = pd.to_datetime(df_r['date'])
-        df_q.index = pd.to_datetime(df_r['date'].values).dt.normalize()
+        df_q.index = pd.to_datetime(df_r['date'].values)
 
         mentions = []
         for _, row in df_r.iterrows():
