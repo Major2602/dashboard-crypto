a8c22819010f8992c75d9f79f97930b4b4ced905
Major2602 <shdm0023@gmail.com>
2026-08-01 15:16:43 +0500

    Update file paths to use BASE_DIR
    Refactor paths to use BASE_DIR for better portability.

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 # CONFIGURATION
 
+# CONFIGURATION
+
+import os
+from pathlib import Path
 import plotly.express as px
 
 class Config:
-  
+    BASE_DIR = Path(__file__).resolve().parent
     TARGET_TICKERS = ['BTC', 'ETH', 'XRP', 'SOL', 'ADA']
-  
-    QUOTES_PATH = 'data/cr_quotes_base_24.csv'
-  
-    REPORT_PATH = 'data/cr_report_24.csv'
-  
-    TICKER_COLORS = {t:  px.colors.qualitative.Plotly[i % len(px.colors.qualitative.Plotly)] for i, t in enumerate(TARGET_TICKERS)}
+    QUOTES_PATH = BASE_DIR / 'data' / 'cr_quotes_base_24.csv'
+    REPORT_PATH = BASE_DIR / 'data' / 'cr_report_24.csv'
+    
+    TICKER_COLORS = {
+        t: px.colors.qualitative.Plotly[i % len(px.colors.qualitative.Plotly)] 
+        for i, t in enumerate(TARGET_TICKERS)
+    }
